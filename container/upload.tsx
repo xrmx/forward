554acdf1cd2ce17133ecb619eba8ce0664048212
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-05-09 18:40:42 +0200

    feat(container): WIP implement upload component

--- container/upload.tsx
+++ container/upload.tsx
@@ -1,26 +1,26 @@
-import * as React from 'react';
-import uuid from 'uuid/v3';
+import * as React from "react";
+import uuid from "uuid/v3";
 
 export const Upload: React.FunctionComponent = (): JSX.Element => {
   const [colorFile, setFile] = React.useState({});
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void  => {
-    const file = e.currentTarget.files[0]
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const file = e.currentTarget.files[0];
     const reader: FileReader = new FileReader();
     reader.onloadend = () => {
-      console.log(reader.result);
-    }
-    setFile(file);
+      setFile(file);
+    };
     reader.readAsText(file);
-  }
+  };
   return (
     <>
       <input
         accept=".json, application/json, text/plain"
-        id={uuid('upload', '1b671a64-40d5-491e-99b0-da01ff1f3341')}
+        id={uuid("upload", "1b671a64-40d5-491e-99b0-da01ff1f3341")}
         multiple={false}
         onChange={handleChange}
         type="file"
-        />
+      />
     </>
-  )
-}
+  );
+};
